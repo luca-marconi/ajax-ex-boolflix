57c2682a3553741a33c5cee8dc7af4145d73ff97
Luca <luca-marconi@hotmail.com>
2020-03-18 11:52:11 +0100

    Inizializzazione

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+
+$(document).ready(function() {
+
+    var apiBaseUrl = 'https://api.themoviedb.org/3';
+
+    $.ajax({
+        // url: 'https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro',
+        // url: 'https://api.themoviedb.org/3/search/movie',
+        url: apiBaseUrl + '/search/movie',
+        data: {
+            api_key: 'e99307154c6dfb0b4750f6603256716d',
+            query: 'ritorno al futuro',
+            language: 'it-IT'
+        },
+        method: 'GET',
+        success: function (data) {
+            console.log(data);
+            var films = data.results;
+            for (var i = 0; i < films.length; i++) {
+                var film = films[i];
+                console.log(film.title);
+            }
+        },
+        error: function (err) {
+            alert('BOOM');
+        }
+    });
+
+});
